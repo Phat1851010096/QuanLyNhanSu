be633151f952b00a60213a91412c26742114ab93
Phat1851010096 <1851010096phat@ou.edu.vn>
2021-09-27 21:10:02 +0700

    Up project QLNhanSu

---
+++ QuanLyNhanSu/DAO/DAO_NhanVien.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu.DAO
+{
+    class DAO_NhanVien
+    {
+        //Phat
+        QLNhanSuEntities db;
+
+        public DAO_NhanVien()
+        {
+            db = new QLNhanSuEntities();
+        }
+
+        public dynamic LayDSNV()
+        {
+            var ds = db.EMPLOYEEs.Select(s => new
+            {
+                s.EMPLOYEEID,
+                s.FULLNAME,
+                s.POSITION.NAME,
+                s.DATESTARTWORKING,
+                s.PHONE,
+                s.ADDRESS,
+                s.BIRTHDATE,
+                s.SEX
+            }).ToList();
+            return ds;
+        }
+
+
+
+
+
+
+
+        //Dang
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Dung
+
+
+
+
+
+
+
+    }
+}
